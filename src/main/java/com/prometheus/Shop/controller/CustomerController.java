2646dae56e1b778c3bc7c411228e281c8ae5af9c
martin.bugar <martin1.bugar@gmail.com>
2020-07-24 14:22:13 +0200

    a

---
+++ src/main/java/com/prometheus/Shop/controller/CustomerController.java
@@ -0,0 +1,46 @@
+package com.prometheus.Shop.controller;
+
+
+import com.prometheus.Shop.db.service.api.CustomerService;
+import com.prometheus.Shop.domain.Customer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("customer")
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @PostMapping
+    public ResponseEntity add(@RequestBody Customer customer){
+        Integer id = customerService.add(customer);
+        if (id != null){
+            return new ResponseEntity<>(id, HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity get (@PathVariable("{id}") int id){
+        Customer customer = customerService.get(id);
+        if (customer == null){
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(customer,HttpStatus.OK);
+
+    }
+
+    @GetMapping()
+    public ResponseEntity getALL (){
+        List<Customer> customerList = customerService.getCustomers();
+        return new ResponseEntity <>(customerList, HttpStatus.OK);
+    }
+}
